f5cfa5b308249c6dfcfa42589090dabcb1615516
roth19 <haktharoth18@gmail.com>
2025-10-18 10:25:32 +0700

    Fix package structure and Docker configuration

--- src/main/java/com/setec/ProductWebApiApplication.java
+++ src/main/java/com/setec/ProductWebApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProductWebApiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ProductWebApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ProductWebApiApplication.class, args);
+    }
 }
