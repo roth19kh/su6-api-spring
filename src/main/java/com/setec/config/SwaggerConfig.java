bf54154a3aa2ea33630e6cdad13733521f2adaa7
roth19 <haktharoth18@gmail.com>
2025-10-18 11:40:18 +0700

    TEMP: Hardcode database credentials to fix deployment

---
+++ src/main/java/com/setec/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.setec.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("SU6 API Spring")
+                        .description("Spring Boot API with Swagger Documentation")
+                        .version("v1.0.0"));
+    }
+}
